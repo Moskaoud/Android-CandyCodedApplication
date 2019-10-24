a1ba037f375f9710321b347819c38aa9f34b678a
Mustafa Muhammad <13590052+Moskaoud@users.noreply.github.com>
2019-10-24 09:22:44 +0200

    complete phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -42,4 +42,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Intent intentCall = new Intent(Intent.ACTION_DIAL);
+        intentCall.setData(Uri.parse("tel:0123456789"));
+        startActivity(intentCall);
+    }
 }
